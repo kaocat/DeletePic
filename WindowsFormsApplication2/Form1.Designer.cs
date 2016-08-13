244d204f5d826b88493ab7c8fb4bbe454d68db43
Kao <a0545157@gmail.com>
2016-08-13 15:41:45 +0800

    First

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tDelDir = new System.Windows.Forms.TextBox();
+            this.tDelDirXml = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnOpenXnl = new System.Windows.Forms.Button();
+            this.oputRes = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tDelDir
+            // 
+            this.tDelDir.Location = new System.Drawing.Point(248, 53);
+            this.tDelDir.Name = "tDelDir";
+            this.tDelDir.Size = new System.Drawing.Size(466, 20);
+            this.tDelDir.TabIndex = 0;
+            // 
+            // tDelDirXml
+            // 
+            this.tDelDirXml.Location = new System.Drawing.Point(248, 110);
+            this.tDelDirXml.Name = "tDelDirXml";
+            this.tDelDirXml.Size = new System.Drawing.Size(466, 20);
+            this.tDelDirXml.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "刪除目錄";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "要刪除的清單";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(120, 348);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "刪除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FilterIndex = 2;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.AccessibleName = "";
+            this.btnOpen.Location = new System.Drawing.Point(736, 49);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "開啟檔案";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnOpenXnl
+            // 
+            this.btnOpenXnl.Location = new System.Drawing.Point(736, 110);
+            this.btnOpenXnl.Name = "btnOpenXnl";
+            this.btnOpenXnl.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenXnl.TabIndex = 6;
+            this.btnOpenXnl.Text = "開啟檔案";
+            this.btnOpenXnl.UseVisualStyleBackColor = true;
+            this.btnOpenXnl.Click += new System.EventHandler(this.btnOpenXnl_Click);
+            // 
+            // oputRes
+            // 
+            this.oputRes.Location = new System.Drawing.Point(117, 162);
+            this.oputRes.Multiline = true;
+            this.oputRes.Name = "oputRes";
+            this.oputRes.ReadOnly = true;
+            this.oputRes.Size = new System.Drawing.Size(694, 167);
+            this.oputRes.TabIndex = 7;
+            this.oputRes.TextChanged += new System.EventHandler(this.oputRes_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(327, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "只支援刪除(jpg/jpeg/png)，使用前請先備份原檔，刪錯不負責";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(752, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "created by kao";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 400);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.oputRes);
+            this.Controls.Add(this.btnOpenXnl);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tDelDirXml);
+            this.Controls.Add(this.tDelDir);
+            this.Name = "Form1";
+            this.Text = "刪圖刪圖";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tDelDir;
+        private System.Windows.Forms.TextBox tDelDirXml;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnOpenXnl;
+        private System.Windows.Forms.TextBox oputRes;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+
+using Microsoft.Office.Interop.Excel;
+using NPOI;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        protected List<string> delList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tDelDir.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnOpenXnl_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "Excel Files|*.xlsx;*.xlsm";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                tDelDirXml.Text = dialog.FileName;
+                openEXcel();
+            }
+        }
+
+        private void oputRes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openEXcel()
+        {
+            string fileExt = Path.GetExtension(tDelDirXml.Text);
+            //Declare the sheet interface
+            ISheet sheet;
+
+            //Get the Excel file according to the extension
+            if (fileExt.ToLower() == ".xls")
+            {
+                //Use the NPOI Excel xls object
+                /*HSSFWorkbook hssfwb;
+                using (FileStream file = new FileStream(tDelDirXml.Text, FileMode.Open, FileAccess.Read))
+                {
+                    hssfwb = new HSSFWorkbook(file);
+                }
+
+                //Assign the sheet
+                sheet = hssfwb.GetSheetAt(0);
+                readExcelToList(sheet);*/
+                MessageBox.Show("不支援xls");
+            }
+            else //.xlsx extension
+            {
+                //Use the NPOI Excel xlsx object
+                XSSFWorkbook hssfwb;
+                using (FileStream file = new FileStream(tDelDirXml.Text, FileMode.Open, FileAccess.Read))
+                {
+                    hssfwb = new XSSFWorkbook(file);
+                }
+
+                //Assign the sheet
+                sheet = hssfwb.GetSheetAt(0);
+                readExcelToList(sheet);
+            }
+
+        }
+
+        private void readExcelToList(ISheet sheet)
+        {
+            XSSFRow hr = (XSSFRow)sheet.GetRow(0);
+            //取得欄位數量
+            int dLastNum = hr.LastCellNum;
+            //有多少筆資料 j=1 有標題,j=0 無標題 (從哪開始讀取資料)
+            for (int j = 0; j <= sheet.LastRowNum; j++)
+            {
+                hr = (XSSFRow)sheet.GetRow(j);
+                //宣告陣列 並將資料同時置入
+                if (hr == null)
+                {
+                    string tempS = "";
+                    delList.Add(tempS);
+                    //oputRes.Text = tempS + "\n";
+                }
+                else
+                {
+                    delList.Add(hr.GetCell(0).ToString());
+                    //oputRes.Text = hr.GetCell(0).ToString() + "\n";
+                }
+                //將每一行的資料存放至arrMyData儲存 ，之後要做什麼應用取得 arrMyData 即可~
+
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DirSearch(tDelDir.Text);
+        }
+
+        public void DirSearch(string sDir)
+        {
+            string[] operators = { "png", "jpg", "jpeg" };
+            
+            if(delList.Count == 0)
+            {
+                MessageBox.Show("請選擇參考用EXCEL或是EXCEL清單格式不符");
+                return;
+            }
+            //DirectoryInfo d = new DirectoryInfo(sDir);
+            foreach (var file in Directory.GetFiles(sDir))
+            {
+                if (operators.Any(x => file.EndsWith(x)))
+                {
+                    string tempName = Path.GetFileNameWithoutExtension(file) + Path.GetExtension(file);
+                    foreach (var key in delList)
+                    {
+                        if (key == tempName)
+                        {
+                            oputRes.Text += "我被刪除了 : " + file + "\r\n";
+                            File.Delete(file);
+                        }
+                    }
+                }
+            }
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                foreach (var file in Directory.GetFiles(d))
+                {
+                    if (operators.Any(x => file.EndsWith(x)))
+                    {
+                        string tempName = Path.GetFileNameWithoutExtension(file) + Path.GetExtension(file);
+                        foreach (var key in delList)
+                        {
+                            if (key == tempName)
+                            {
+                                oputRes.Text += "我被刪除了 : " + file + "\r\n";
+                                File.Delete(file);
+                            }
+                        }
+                    }
+                }
+                DirSearch(d);
+            }       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
